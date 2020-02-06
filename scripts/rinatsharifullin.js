c453c17a20da09e7a42aa76e36fd4994926fdd5c
rinatsharifullin <58179621+rinatsharifullin@users.noreply.github.com>
2020-02-06 19:34:15 +0000

    Add files via upload

--- scripts/rinatsharifullin.js
+++ scripts/rinatsharifullin.js
@@ -5,18 +5,21 @@ document.addEventListener('DOMContentLoaded', function(){
 	var pElement;
 	var aElement;
 	var textElement;
-	var myName=["R","i","n","a","t"," ","S","h","a","r","i","f","u","l","l","i","n"];
+	//var myName=["R","i","n","a","t"," ","S","h","a","r","i","f","u","l","l","i","n"];
+	var myName=["H","i",","," ","I","'","m"," ","R","i","n","a","t","."," ","I"," ","F","r","o","n","t"," ","E","n","d"," ","W","e","b"," ","D","e","v","e","l","o","p","e","r","."];
+	/*Hi, I'm Rinat. I Front End Web Developer.*/
 	var myTimer;
 
 	pElement = document.createElement('p');			//Create P tag
 	pElement.className="rinatsharifullin";
+	pElement.style.fontSize = '5em';
 	document.body.appendChild(pElement);
 	var q=0;
 	var interval = setInterval(function(){
 		
 			textElement = document.createTextNode(myName[q]);
 			
-			console.log(textElement);
+			//console.log(textElement);
 			pElement.appendChild(textElement);
 			
 			q++;
