946199eba709f05319886d5e0f5688f85bafc866
rinatsharifullin <58179621+rinatsharifullin@users.noreply.github.com>
2020-02-13 20:45:24 +0000

    Add files via upload

--- scripts/populatescripts.js
+++ scripts/populatescripts.js
@@ -1,44 +1,74 @@
 
 
 document.addEventListener('DOMContentLoaded', function(){
+	provideHTMLandStyle();
 	window.addEventListener('resize', styleOnResize);
-	document.getElementById('browserSize').innerHTML  = ('width ' + window.innerWidth + ', height ' + window.innerHeight);
-  populateContent();
+	document.getElementById('mobileicon').addEventListener('click', function(){
+		if(document.getElementById('myNav').style.display == 'none'){
+			document.getElementById('myNav').style.display = 'block';
+			document.getElementById('mobileicon').innerHTML='&#9932';
+		}else{
+			document.getElementById('myNav').style.display = 'none';
+			document.getElementById('mobileicon').innerHTML='&#9776';
+		}
+	})
+	//document.getElementById('browserSize').innerHTML  = ('width: ' + window.innerWidth + ', height: ' + window.innerHeight);
 });
 
 
 function styleOnResize(){
-	document.getElementById('browserSize').innerHTML  = ('width ' + window.innerWidth + ', height ' + window.innerHeight);
-	console.log(document.getElementById('browserSize').innerHTML );
+	//document.getElementById('browserSize').innerHTML  = ('width: ' + window.innerWidth + ', height: ' + window.innerHeight);
+	if(window.innerWidth>'985'){
+		styleOver985();
+	}else{
+		styleUnder985();
+	}
 }
 
+function styleUnder985(){
 
+	/*Styling A tags*/
+	var aTags = document.getElementsByClassName("nav");
+	for(var position = 0; position<aTags.length; position++){
+		aTags[position].style.fontSize = '2em';
+		aTags[position].style.display = 'block';
+		aTags[position].style.width = '100%';
+	}
+	/*Show mobile navigation icon elements*/
+	var navElement = document.getElementById('mobileicon');
+	navElement.style.display='block';
+	//navElement.style.position='absolute';
+	navElement.style.fontSize='3vw';
+	document.getElementById('myNav').style.display = 'none';
+	navElement.onmouseover = function(){
+		this.style.cursor = 'pointer';
 
-/* Keep this function declaration for future projects. Don't need to remove it, 
-   even if it doesn't do anything for this exercise.  */
+		}
+	navElement.onmouseout = function(){
+		this.style.cursor = 'auto';
+		}
 
-/*
-const p = document.createElement("p");
-p.textContent = "test1";
-document.body.insertBefore(p, document.body.firstChild);
-*/
+}
 
-
-
-
-function populateContent() {
-	/*Manage navigation paths depends of from what page*/
-	/*var path = window.location.pathname;
-	var page = path.split("/").pop();
-	console.log(page);
-	var prefix="";
-	var prefix2 = "html/";
-	if(page != "index.html"){prefix = "../"; prefix2 = "";}*/
+function styleOver985(){
+	/*Styling A tags*/
+	var aTags = document.getElementsByClassName("nav");
+	for(var position = 0; position<aTags.length; position++){
+		aTags[position].style.display = 'inline-block';
+		aTags[position].style.width = '150px';
+		aTags[position].style.fontSize = '1em';
+	}
+	/*Show mobile navigation icon elements*/
+	var navElement = document.getElementById('mobileicon');
+	navElement.style.display='none';
 	
-	/*Create navigation elements*/
+	document.getElementById('myNav').style.display = 'block';
+}
+function provideHTMLandStyle(){
 	var navElement;
 	var pElement;
 	var aElement;
+	/*Create navigation elements*/
 	navElement = document.createElement('nav');
 	navElement.id = "myNav";
 	pElement = document.createElement('p');			//Create P tag
@@ -90,7 +120,13 @@ function populateContent() {
 	
 	navElement.appendChild(pElement);
 	document.body.insertBefore(navElement, document.body.firstChild);
-	//document.getElementById("myNav").appendChild(pElement);	//Append P tag to our HTML nav tag.
+	
+	/*Create mobile navigation icon elements*/
+	navElement = document.createElement('nav');
+	navElement.id = "mobileicon";
+	navElement.innerHTML='&#9776';
+	navElement.style.display='none';
+	document.body.insertBefore(navElement, document.body.firstChild);
 	
 	/*Styling Body*/
 	var myBody = document.body;
@@ -98,7 +134,6 @@ function populateContent() {
 	myBody.style.padding = '0';
 	myBody.style.maxWidth = '1080px';
 	myBody.style.fontFamily = 'font-family: "Lato", sans-serif';
-	
 	
 	/*Style nav*/
 	var myNav = document.getElementById("myNav");
@@ -113,11 +148,8 @@ function populateContent() {
 		aTags[position].style.backgroundColor = 'white';
 		aTags[position].style.textDecoration = "none";
 		aTags[position].style.color = 'gray';
-		aTags[position].style.display = 'inline-block';
-		aTags[position].style.width = '150px';
 		aTags[position].style.textAlign = 'center';
-		aTags[position].style.fontSize = '1em';
-		
+
 		aTags[position].onmouseover = function(){
 			this.style.boxShadow = "0 1px 6px 0 rgba(32,33,36,0.28)";
 			this.style.textDecoration = "underline";
@@ -132,39 +164,12 @@ function populateContent() {
 			this.style.color = "gray";
 			}
 	}
-
 	
-	
-	/*
-	border:solid #dfe1e5 1px;
-	padding:7px 12px;
-	border-radius:15px;
-	text-decoration:none;*/
-
-	
-	/*
-	var node;
-	var pnode;
-	var textnode;
-	for (var numb=1; numb<25; numb++){
-		node = document.createElement("div");
-		pnode = document.createElement('p');
-		textnode = document.createTextNode(numb);
-		pnode.appendChild(textnode);
-		node.appendChild(pnode);
-		document.getElementById("MySec").appendChild(node);
-			
-		node.className="square";
-		pnode.className="numbers";
-			
-		node.style.width="33px";
-		node.style.height="33px";
-		node.style.border="silver solid 0.5px";
-		node.style.textAlign="center";
-		node.style.display="inline-block";
-		node.style.margin="15px";
-		pnode.style.position="relative";
-		pnode.style.top="20px";
-	}*/
-		
+	if(window.innerWidth>'985'){
+		styleOver985();
+	}else{
+		styleUnder985();
+	}
 }
+
+
